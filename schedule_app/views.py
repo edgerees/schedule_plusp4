14e728e7c0a0300ffffc8818c5e843a25fa84cd7
Edree Huss <edreeshuss@gmail.com>
2021-01-20 14:31:15 -0500

    setup pages, layouts, django.

--- schedule_app/views.py
+++ schedule_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request,'schedule_app/dashboard.html')
+
+def tasks(request):
+    return render(request,'schedule_app/tasks.html')
+
+def employee(request):
+    return render(request,'schedule_app/employee.html')
+
