d86f14d8f9ae492664407767b18151901b5d3a95
Edree Huss <edreeshuss@gmail.com>
2021-01-21 15:03:34 -0500

    login-setup admin login

--- schedule_app/urls.py
+++ schedule_app/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
+
+    path('', views.home, name="home"),
     path('positions/', views.positions),
     path('employee/<str:pk>/', views.employee),
 ]
