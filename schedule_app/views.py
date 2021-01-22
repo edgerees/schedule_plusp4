faefd5506869e5aefd27a4a89dd988dfb63a93d4
Edree Huss <edreeshuss@gmail.com>
2021-01-22 15:36:04 -0500

    permissions check

--- schedule_app/views.py
+++ schedule_app/views.py
@@ -5,59 +5,67 @@ from .models import *
 from .forms import TaskForm
 from .filters import TaskFilter
 from django.contrib.auth.forms import UserCreationForm
+
 from django.contrib.auth import authenticate, login, logout
+
 from django.contrib import messages
+
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 
 from .forms import CreateUserForm
+from .decorators import unauthenticated_user, allowed_users, admin_only
 
+@unauthenticated_user
 def landing(request):
     return render(request, 'schedule_app/landing.html')
 
+@unauthenticated_user
 def registerPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
 
-                return redirect('login')
+	form = CreateUserForm()
+	if request.method == 'POST':
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
 
-        context = {'form': form}
-        return render(request, 'schedule_app/register.html', context)
+			group = Group.objects.get(name='employees')
+			user.groups.add(group)
 
+			messages.success(request, 'Account was created for ' + username)
 
+			return redirect('login')
+		
+
+	context = {'form':form}
+	return render(request, 'schedule_app/register.html', context)
+
+@unauthenticated_user
 def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
 
-            user = authenticate(request, username=username, password=password)
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password =request.POST.get('password')
 
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.info(request, 'Username OR password is incorrect')
+		user = authenticate(request, username=username, password=password)
 
-        context = {}
-        return render(request, 'schedule_app/login.html', context)
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			messages.info(request, 'Username OR password is incorrect')
 
+	context = {}
+	return render(request, 'schedule_app/login.html', context)
 
 def logoutUser(request):
-    logout(request)
-    return redirect('login')
+	logout(request)
+	return redirect('login')
 
 
-@login_required(login_url='landing')
+@login_required(login_url='login')
+@admin_only
 def home(request):
     tasks = Task.objects.all()
     employees = Employee.objects.all()
