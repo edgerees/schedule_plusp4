056b65901c9fef44350235d4dc1f35e20b5f7da3
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-01-28 06:35:03 -0600

    Add push to heroku

--- schedule_plus/settings.py
+++ schedule_plus/settings.py
@@ -82,18 +82,18 @@ ASGI_APPLICATION = 'schedule_plus.routing.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Task_Manager',
+        'USER': 'postgres',
+        'PASSWORD': 'tasksrcool',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
 # 'default': {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'NAME': 'Task_Manager',
-#     'USER': 'postgres',
-#     'PASSWORD': 'tasksrcool',
-#     'HOST': 'localhost',
-#     'PORT': '5432'
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 # }
 
 # Password validation
