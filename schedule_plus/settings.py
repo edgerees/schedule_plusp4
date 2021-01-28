ee236c3773d56be9b145d8d530aa3fd3606fa8eb
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-01-28 10:31:04 -0600

    fix heroku

--- schedule_plus/settings.py
+++ schedule_plus/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
     #     'PORT': '5432'
     # }
 }
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'.update(db_from_env)]
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'.update(db_from_env)]
 
 
 # Password validation
